3233c3dfc3f4b0e9f4cdc27dff38f5550cb00390
Akash <sadawarti.akash@gmail.com>
2023-12-18 16:56:27 +0530

    hello world aws

---
+++ hello-world.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/sample", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    author: "Akash",
+    description: "Hello World, Welcome to Amazon web sevices",
+  });
+});
+
+
+app.listen(5000, () => {
+  console.log("Hello I am node server");
+});
